38b3efad84b5e0adc7e4b329cbc1645f2793ee0a
Tran Dinh Gia Huy <trandinhgiahuy2004@gmail.com>
2025-06-29 01:51:14 +0700

    fix

---
+++ Assets/StateMachine/Boss_Fire.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Boss_Fire : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.GetComponent<BossHealth>().isInvulnerable = true; // Set invulnerable state    
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.GetComponent<BossHealth>().isInvulnerable = false; // Reset invulnerable state
+    }
+
+
+}
